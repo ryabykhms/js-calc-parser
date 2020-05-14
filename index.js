d204b92e9c95afb7eea0ec2af0aff45859309b5f
upstrocker <upstrocker@gmail.com>
2020-05-14 18:05:31 +0300

    Implemented entry point for calculate

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const { Lexer } = require('./src/parser/Lexer');
 const { Parser } = require('./src/parser/Parser');
+const { Calc } = require('./Calc');
 
+const calc = new Calc();
+console.log(calc.calculate('(E+2.876+2)*2'));
 // const input = '(E+2.876+2)*2';
-const input = 'percent(10,2)';
-const lexer = new Lexer(input);
-const tokens = lexer.tokenize();
-console.log(tokens);
 
-const parser = new Parser(tokens);
-const expressions = parser.parse();
-expressions.forEach(expression => {
-  console.log(expression + ' = ' + expression.evaluate());
-});
+// const input = 'percent(10,2)';
+// const lexer = new Lexer(input);
+// const tokens = lexer.tokenize();
+// console.log(tokens);
+//
+// const parser = new Parser(tokens);
+// const expressions = parser.parse();
+// expressions.forEach(expression => {
+//   console.log(expression + ' = ' + expression.evaluate());
+// });
+
